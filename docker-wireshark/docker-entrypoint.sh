8f664e0ae64482572faadf5dfe7f30c568606cc8
Wolfgang Tremmel <wolfgang.tremmel@de-cix.net>
2021-04-30 13:44:08 +0200

    initial commit

---
+++ docker-wireshark/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# start inetd
+/usr/sbin/inetd
+
+# start xpra as xpra user with command specified in dockerfile as CMD or passed as parameter to docker run
+CMD="XPRA_PASSWORD=$XPRA_PASSWORD /usr/bin/xpra start --daemon=no --start-child='$@'"
+runuser -l xpra -c "$CMD"
